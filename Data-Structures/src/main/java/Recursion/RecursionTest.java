bd6027dd4043d761f1e46ea03f52806655527c9c
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-29 23:54:03 +0300

    playing with recursion

---
+++ Data-Structures/src/main/java/Recursion/RecursionTest.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class RecursionTest {
+    public static void main(String[] args) {
+
+    }
+    public void printAllPer(char[] array){
+        if (array.length > 1) {
+            
+        }else if (array.length == 1){
+            System.out.println(array[0]);
+        }
+    }
+
+}
