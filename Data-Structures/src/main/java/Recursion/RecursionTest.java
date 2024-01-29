86c6d8f64330aea62c156b682c2f1af03a30505a
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-30 00:33:36 +0300

    implementation of recursive algorithm to print all permutations of a list of strings

--- Data-Structures/src/main/java/Recursion/RecursionTest.java
+++ Data-Structures/src/main/java/Recursion/RecursionTest.java
@@ -1,15 +1,26 @@
 package Recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class RecursionTest {
     public static void main(String[] args) {
-
+        printAllPer("",(List<String>) Arrays.asList("a","b","c"));
     }
-    public void printAllPer(char[] array){
-        if (array.length > 1) {
-            
-        }else if (array.length == 1){
-            System.out.println(array[0]);
+    public static void printAllPer(String s ,List<String> array){
+        if (array.size() > 1) {
+            for(String c : array){
+                List<String> temp = copy(array);
+                temp.remove(c);
+                printAllPer(s+c,temp);
+            }
+        }else if (array.size() == 1){
+            System.out.println(s.concat(String.valueOf(array.get(0))));
         }
     }
 
+    private static <T> List<T> copy(List<T> source){
+        return new ArrayList<>(source);
+    }
 }
