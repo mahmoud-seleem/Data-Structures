f2da62741f7b416ecec4d0ce636c0144e7c014a5
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-30 19:05:09 +0300

    testing the circular queue

--- Data-Structures/src/main/java/Queue/QueueTest.java
+++ Data-Structures/src/main/java/Queue/QueueTest.java
@@ -2,25 +2,13 @@ package Queue;
 
 public class QueueTest {
     public static void main(String[] args) {
-        LinkedQueue<Integer> queue = new LinkedQueue<Integer>();
-        queue.enqueue(10);
-        queue.enqueue(5);
-        queue.enqueue(11);
-        queue.dequeue();
-        queue.enqueue(12);
-        queue.enqueue(4);
-        queue.dequeue();
-        queue.dequeue();
-        queue.dequeue();
-        queue.dequeue();
-        queue.enqueue(555);
+        CircularLinkedQueue<Integer> queue = new CircularLinkedQueue<Integer>();
         queue.enqueue(5555);
-        queue.dequeue();
         queue.enqueue(5);
         queue.enqueue(55);
         queue.enqueue(56);
         System.out.println(queue);
-        queue.dequeue();
+        queue.rotate();
         System.out.println(queue);
     }
 }
