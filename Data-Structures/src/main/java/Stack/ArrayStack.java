ae9ee9151148a0b5b57e331ac5bfc62fd9d20215
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-30 14:08:58 +0300

    implement stack using array

---
+++ Data-Structures/src/main/java/Stack/ArrayStack.java
@@ -0,0 +1,28 @@
+package Stack;
+
+public class ArrayStack<E> implements Stack<E>{
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void push(E element) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public E top() {
+        return null;
+    }
+}
