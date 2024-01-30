42c8e3742a2388cfa8f0221d348747a37fd25c21
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-30 15:01:06 +0300

    array stack is about 3.5 times faster than linked stack

--- Data-Structures/src/main/java/Stack/StackTest.java
+++ Data-Structures/src/main/java/Stack/StackTest.java
@@ -3,16 +3,23 @@ package Stack;
 public class StackTest {
     public static void main(String[] args) {
         LinkedStack<Integer> stack = new LinkedStack<Integer>();
+        ArrayStack<Integer> arrayStack = new ArrayStack<Integer>(100);
+        long start = System.nanoTime();
         stack.push(5);
-        stack.push(10);
-        System.out.println(stack);
-        System.out.println("-" +stack.pop()+"-");
-        System.out.println("="+stack.top() + "=");
-        System.out.println(stack);
-        System.out.println(stack.size());
-        stack.pop();
-        System.out.println(stack.size());
-        System.out.println(stack.isEmpty());
+        long end = System.nanoTime();
+        System.out.println("linked stack takes time = " + (end - start));
+        start = System.nanoTime();
+        arrayStack.push(5);
+        end = System.nanoTime();
+        System.out.println("array stack takes time = "+(end - start));
+//        System.out.println(stack);
+//        System.out.println("-" +stack.pop()+"-");
+//        System.out.println("="+stack.top() + "=");
+//        System.out.println(stack);
+//        System.out.println(stack.size());
+//        stack.pop();
+//        System.out.println(stack.size());
+//        System.out.println(stack.isEmpty());
 
     }
 }
