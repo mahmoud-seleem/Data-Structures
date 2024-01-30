abcba3fd060ba6f03ecb205fb59660b8c8445905
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2024-01-30 15:02:32 +0300

    while poping take almost the same time for both

--- Data-Structures/src/main/java/Stack/StackTest.java
+++ Data-Structures/src/main/java/Stack/StackTest.java
@@ -4,12 +4,14 @@ public class StackTest {
     public static void main(String[] args) {
         LinkedStack<Integer> stack = new LinkedStack<Integer>();
         ArrayStack<Integer> arrayStack = new ArrayStack<Integer>(100);
-        long start = System.nanoTime();
         stack.push(5);
+        arrayStack.push(5);
+        long start = System.nanoTime();
+        stack.pop();
         long end = System.nanoTime();
         System.out.println("linked stack takes time = " + (end - start));
         start = System.nanoTime();
-        arrayStack.push(5);
+        arrayStack.pop();
         end = System.nanoTime();
         System.out.println("array stack takes time = "+(end - start));
 //        System.out.println(stack);
